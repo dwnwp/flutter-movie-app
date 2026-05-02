d592896a08c46f5db41ca63123a858f8b50ac543
dwnwp <dewsinglor@gmail.com>
2026-05-02 16:55:25 +0700

    Fix trailer video is not display

--- lib/function/trailerui.dart
+++ lib/function/trailerui.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 class trailerwatch extends StatefulWidget {
-  var trailerytid;
-  trailerwatch(this.trailerytid, {super.key});
+  final String trailerytid;
+  const trailerwatch(this.trailerytid, {super.key});
 
   @override
   State<trailerwatch> createState() => _trailerwatchState();
@@ -11,52 +11,33 @@ class trailerwatch extends StatefulWidget {
 
 class _trailerwatchState extends State<trailerwatch> {
   late YoutubePlayerController _controller;
+
   @override
   void initState() {
-    final videoid = YoutubePlayer.convertUrlToId(widget.trailerytid);
-    _controller = YoutubePlayerController(
-      initialVideoId: videoid.toString(),
-      flags: const YoutubePlayerFlags(
-        enableCaption: true,
-        autoPlay: false,
+    super.initState();
+    _controller = YoutubePlayerController.fromVideoId(
+      videoId: widget.trailerytid,
+      autoPlay: false,
+      params: const YoutubePlayerParams(
+        showControls: true,
         mute: false,
-        forceHD: true,
+        showFullscreenButton: true,
+        loop: false,
       ),
     );
   }
 
   @override
+  void dispose() {
+    _controller.close();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(0.0),
-      child: YoutubePlayer(
-        thumbnail: Image.network(
-          "${"https://img.youtube.com/vi/" + widget.trailerytid}/hqdefault.jpg",
-          fit: BoxFit.cover,
-        ),
-        controlsTimeOut: const Duration(milliseconds: 1500),
-        aspectRatio: 16 / 9,
-        controller: _controller,
-        showVideoProgressIndicator: true,
-        bufferIndicator: const Center(
-          child: Center(
-            child: CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation(Colors.blue),
-            ),
-          ),
-        ),
-        progressIndicatorColor: Colors.blue,
-        bottomActions: [
-          CurrentPosition(),
-          ProgressBar(
-            isExpanded: true,
-            colors: const ProgressBarColors(
-                playedColor: Colors.white, handleColor: Colors.blue),
-          ),
-          RemainingDuration(),
-          FullScreenButton(),
-        ],
-      ),
+    return YoutubePlayer(
+      controller: _controller,
+      aspectRatio: 16 / 9,
     );
   }
 }
